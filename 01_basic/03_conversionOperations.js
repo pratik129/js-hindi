0b9b2d237b4fef11cded107dbafc615588c61cca
Pratik chavan <pratikchm123@gmail.com>
2024-07-01 17:47:10 +0530

    conversion

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = "pratik22"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+//------------------------------------
+
+// let isCheckedIn = "Ramesh"
+
+
+let isCheckedIn = "2"
+let isBool = Boolean(isCheckedIn)
+// console.log(typeof isBool)
+// console.log(isBool)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let strinNumber = String(someNumber)
+console.log(typeof strinNumber)
+console.log(strinNumber)
