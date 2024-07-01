b7bf518b28a85b3ea41673d4a813869498400edb
Pratik chavan <pratikchm123@gmail.com>
2024-07-01 18:10:46 +0530

    Operations

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -28,5 +28,52 @@ let isBool = Boolean(isCheckedIn)
 let someNumber = 33
 
 let strinNumber = String(someNumber)
-console.log(typeof strinNumber)
-console.log(strinNumber)
+// console.log(typeof strinNumber)
+// console.log(strinNumber)
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+
+// console.log(2*2)
+
+// console.log(2/2)
+
+// console.log(2**2)
+
+// console.log(2%2)
+
+let fname = "Pratik"
+let lName = " Chavan"
+let fullName = fname + lName
+// console.log(fullName)
+
+
+console.log(2+"2")
+console.log("2"+2)
+console.log("2"+2+1)
+console.log(1+2+"2")
+
+
+console.log(+true)
+console.log(+"")
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 //not a perfeact way
+
+let visitCounter =  100
+++visitCounter;
+console.log(visitCounter)
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
